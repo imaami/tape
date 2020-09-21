f8140874007ee52e29b5b2a4ea518b8cf8bf62ff
Juuso Alasuutari <juuso.alasuutari@gmail.com>
2020-09-21 15:54:25 +0300

    Add options.h files to the modules in example, not used for anything yet

---
+++ example/grid/options.h
@@ -0,0 +1,11 @@
+#ifndef TAPE__OPTIONS_H__INCLUDED_
+#define TAPE__OPTIONS_H__INCLUDED_
+
+// TODO: Not actually used yet
+
+/*     Tag      -x      --xxx           Arg.    Help text           */
+OPTION(Rows,    'r',    "rows",         1,      "Number of rows")
+OPTION(Columns, 'c',    "columns",      1,      "Number of columns")
+OPTION(Help,    'h',    "help",         0,      "Show this help text")
+
+#endif /* TAPE__OPTIONS_H__INCLUDED_ */
---
+++ example/wait/options.h
@@ -0,0 +1,11 @@
+#ifndef TAPE__OPTIONS_H__INCLUDED_
+#define TAPE__OPTIONS_H__INCLUDED_
+
+// TODO: Not actually used yet
+
+/*     Tag      -x      --xxx           Arg.    Help text           */
+OPTION(Seconds, 's',    "seconds",      1,      "Wait time in seconds")
+OPTION(Minutes, 'm',    "minutes",      1,      "Wait time in minutes")
+OPTION(Help,    'h',    "help",         0,      "Show this help text")
+
+#endif /* TAPE__OPTIONS_H__INCLUDED_ */
