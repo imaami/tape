e20e41f5c4e3fa647b38d154c4050421f54b6bc2
Juuso Alasuutari <juuso.alasuutari@gmail.com>
2020-09-21 14:56:36 +0300

    Move common make functions and variables to common.mk

--- common.mk
+++ common.mk
@@ -14,6 +14,16 @@ endif # __COMMON_MK_INCLUDED
 ## DO NOT ADD ANYTHING ABOVE THIS LINE ##
 #########################################
 
+override HEAD_CFLAGS := -std=gnu11 -Wall -Wextra
+override TAIL_CFLAGS := $(if $(strip $(CFLAGS)),$(CFLAGS),-O3 -flto)
+
+override HEAD_CXXFLAGS := -std=c++17 -Wall -Wextra -pedantic
+override TAIL_CXXFLAGS := $(if $(strip $(CXXFLAGS)),$(CXXFLAGS),-O3 -flto)
+
+override IFLAGS := -I. -I$(PROJECT_ROOT)include
+override CFLAGS := $(HEAD_CFLAGS) $(TAIL_CFLAGS)
+override CXXFLAGS := $(HEAD_CXXFLAGS) $(TAIL_CXXFLAGS)
+
 override define __recurse =
 	target=$(strip $(2)); \
 	for subdir in $(3); do \
@@ -30,12 +40,31 @@ $(strip $(1)):
 	+$(call __recurse,exit 1,$(strip $(2)),$(strip $(3)))
 endef
 
-export build-subdirs
-
 override define clean-subdirs =
 .PHONY: $(strip $(1))
 $(strip $(1)):
 	+$(call __recurse,true,$(strip $(2)),$(strip $(3)))
 endef
 
+override define build-c-obj =
+.PHONY: $$(notdir $(1))
+$$(notdir $(1)): $(1)
+
+$(1): $(1:.c.o=.c)
+	cd "$$(DIR)" && \
+	$$(CC) $$(CFLAGS) $(IFLAGS) -c $$(notdir $$^) -o $$(notdir $$@)
+endef
+
+override define build-cxx-obj =
+.PHONY: $$(notdir $(1))
+$$(notdir $(1)): $(1)
+
+$(1): $(1:.cpp.o=.cpp)
+	cd "$$(DIR)" && \
+	$$(CXX) $$(CXXFLAGS) $(IFLAGS) -c $$(notdir $$^) -o $$(notdir $$@)
+endef
+
+export build-subdirs
 export clean-subdirs
+export build-c-obj
+export build-cxx-obj
